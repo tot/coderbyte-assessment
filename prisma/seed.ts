664fea3a6276b32db33e637d938eed64ed7978b5
Tony <totran0331@gmail.com>
2023-10-28 17:45:08 -0400

    Fixed seed script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,25 +3,15 @@ const prisma = new PrismaClient();
 
 async function reset() {
   console.info(`[${new Date().toLocaleTimeString()}] Resetting database...`);
+  await prisma.post.deleteMany({});
   console.info(`[${new Date().toLocaleTimeString()}] Done resetting database!`);
 }
 
 async function main() {
-  const testAuthor = await prisma.author.create({
-    data: {
-      name: "Johnny",
-    },
-  });
-
-  console.info(
-    `[${new Date().toLocaleTimeString()}] Created new user: `,
-    testAuthor,
-  );
-
   const testPost = await prisma.post.create({
     data: {
       title: "My First Blog Post",
-      authorId: testAuthor.id,
+      author: "Johnny Test",
       content:
         "Curabitur elementum massa mauris, et scelerisque enim mollis in. In venenatis magna lobortis, hendrerit magna eget, elementum erat. Fusce dignissim vestibulum nunc a condimentum. Aliquam rhoncus finibus finibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris congue leo lacinia nulla tristique, quis rhoncus quam rhoncus. Maecenas fringilla purus in venenatis mattis. Vestibulum pretium eros lacus, elementum aliquam est feugiat sit amet.",
       slug: "first-blog-post",
