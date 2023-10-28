f803095ca78ce9ea6951456a5c244cafe5a77907
Tony <totran0331@gmail.com>
2023-10-28 10:41:16 -0400

    Created UI components; created initial page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,19 @@
 import { type AppType } from "next/app";
+import { Inter } from "next/font/google";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import { cn } from "~/lib/utils";
+
+const inter = Inter({ subsets: ["latin"] });
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <main className={cn(inter.className, "bg-neutral-100")}>
+      <Component {...pageProps} />
+    </main>
+  );
 };
 
 export default api.withTRPC(MyApp);
