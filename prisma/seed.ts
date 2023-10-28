f0dd5c3c400b3525f2c07c4e49f8d3107317953d
Tony <totran0331@gmail.com>
2023-10-28 17:36:30 -0400

    Updated Prisma schema and seeding

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,5 @@
-import { db } from "~/server/db";
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 async function reset() {
   console.info(`[${new Date().toLocaleTimeString()}] Resetting database...`);
@@ -6,7 +7,7 @@ async function reset() {
 }
 
 async function main() {
-  const testAuthor = await db.author.create({
+  const testAuthor = await prisma.author.create({
     data: {
       name: "Johnny",
     },
@@ -16,6 +17,21 @@ async function main() {
     `[${new Date().toLocaleTimeString()}] Created new user: `,
     testAuthor,
   );
+
+  const testPost = await prisma.post.create({
+    data: {
+      title: "My First Blog Post",
+      authorId: testAuthor.id,
+      content:
+        "Curabitur elementum massa mauris, et scelerisque enim mollis in. In venenatis magna lobortis, hendrerit magna eget, elementum erat. Fusce dignissim vestibulum nunc a condimentum. Aliquam rhoncus finibus finibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris congue leo lacinia nulla tristique, quis rhoncus quam rhoncus. Maecenas fringilla purus in venenatis mattis. Vestibulum pretium eros lacus, elementum aliquam est feugiat sit amet.",
+      slug: "first-blog-post",
+    },
+  });
+
+  console.info(
+    `[${new Date().toLocaleTimeString()}] Created new post: `,
+    testPost,
+  );
 }
 
 reset()
@@ -23,10 +39,10 @@ reset()
     await main();
   })
   .then(async () => {
-    await db.$disconnect();
+    await prisma.$disconnect();
   })
   .catch(async (e) => {
     console.error(e);
-    await db.$disconnect();
+    await prisma.$disconnect();
     process.exit(1);
   });
