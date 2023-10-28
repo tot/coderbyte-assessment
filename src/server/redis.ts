33e08c3c79cbde6f6b977453521e0c97ef80749b
Tony <totran0331@gmail.com>
2023-10-28 03:54:55 -0400

    Added Redis support for caching

---
+++ src/server/redis.ts
@@ -0,0 +1,42 @@
+import Redis, { RedisOptions } from "ioredis";
+import { env } from "~/env.mjs";
+
+export function createRedisInstance(
+  config = {
+    host: env.REDIS_URL,
+    port: parseInt(env.REDIS_PORT),
+  },
+) {
+  try {
+    const options: RedisOptions = {
+      host: config.host,
+      lazyConnect: true,
+      showFriendlyErrorStack: true,
+      enableAutoPipelining: true,
+      maxRetriesPerRequest: 0,
+      retryStrategy: (times: number) => {
+        if (times > 3) {
+          throw new Error(`[Redis] Could not connect after ${times} attempts`);
+        }
+
+        return Math.min(times * 200, 1000);
+      },
+    };
+
+    if (config.port) {
+      options.port = config.port;
+    }
+
+    const redis = new Redis(options);
+
+    redis.on("error", (error: unknown) => {
+      console.warn("[Redis] Error connecting", error);
+    });
+
+    return redis;
+  } catch (e) {
+    throw new Error(`[Redis] Could not create a Redis instance`);
+  }
+}
+
+export const redis = createRedisInstance();
